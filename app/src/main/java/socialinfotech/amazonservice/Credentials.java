290d352b280d9a03244f02ac1f66928c6967ed07
Pankaj_SavaliyaGit <pankaj.savaliya1111@gmail.com>
2016-07-06 11:27:13 +0530

    Credentials Set

--- app/src/main/java/socialinfotech/amazonservice/Credentials.java
+++ app/src/main/java/socialinfotech/amazonservice/Credentials.java
@@ -4,11 +4,11 @@ package socialinfotech.amazonservice;
  * Created by pankaj on 05/07/16.
  */
 public class Credentials {
-    public static final String HOST = "https://n4g08yp8j9.execute-api.us-west-2.amazonaws.com";
+    public static final String HOST = "https://example.execute-api.us-west-2.amazonaws.com";
     public static final String CONTENT_TYPE="application/x-www-form-urlencoded";
-    public static final String LOGIN_URL = "https://n4g08yp8j9.execute-api.us-west-2.amazonaws.com/bookmix/login";
-    public static final String AccessKey = "AKIAIVP6KFXJFHWXD7HQ";
-    public static final String Secret_key = "QEK+FFrEvnf36si9vxj9d32kLU7clFcYOUfPhnjA";
+    public static final String LOGIN_URL = "https://example.execute-api.us-west-2.amazonaws.com/bookmix/login";
+    public static final String AccessKey = "AKIAIVP6fghffFHWXD7HQ";
+    public static final String Secret_key = "QEK+FFrEvnf36dfgdfg9d32kLU7clFcYOUfPhnjA";
     public static final String AWS_Region = "us-west-2";
     public static final String Service_Name = "execute-api";
 
